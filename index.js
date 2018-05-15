de4b67edc6bc73baa1a720c64957dcd794e29e66
Kevin Bouwmeester <noreply@gmail.com>
2018-05-15 11:58:01 +0000

    Typo in comment

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ console.log("BaaS migration tool");
 console.log("===");
 console.log("Reading data from BaaS...");
 baas.export(function(data) {
-		/* Writign data to file optional for debugging 
+		/* OPTIONAL - Writing data to file for debugging 
  		fs.writeFile("./data.json", JSON.stringify(data), 'utf8', function(err) {
                 	if(err) throw err;
                 	console.log('4. Saved data.json!');
